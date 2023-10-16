dddd3af7b90428b00fb85be7dc272d64c4e6ab4b
didine712 <aguenassia@gmail.com>
2023-10-17 00:01:37 +0200

    top tracks

--- server/api.py
+++ server/api.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, HTTPException
+from sqlalchemy import func
 from .database import SessionLocal
 from fastapi import Depends
 from . import models
 
 app = FastAPI()
 
-# Dépendance pour la gestion de la session de base de données
+
+# Database session dependency
 def get_db():
     db = SessionLocal()
     try:
@@ -13,40 +15,78 @@ def get_db():
     finally:
         db.close()
 
+
 @app.get("/genres/")
-def read_genres(db: SessionLocal=Depends(get_db)):
+def read_genres(db: SessionLocal = Depends(get_db)):
     """
-    Récupère la liste des genres depuis la base de données.
+    Get the list of genres from the database.
     """
     genres = db.query(models.Genres).all()
     return genres
 
+
 @app.get("/artists/{name}")
-async def get_artists_by_name(name: str,db: SessionLocal=Depends(get_db)):
+async def get_artists_by_name(artist_name: str, db: SessionLocal = Depends(get_db)):
     """
-    Récupère la liste d'artistes dont le nom contient la chaîne donnée.
+    Get a list of artists whose name contains the given string.
     """
-    artists = db.query(models.Artists).filter(models.Artists.Name.like(f"%{name}%")).all()
-    if(artists):
-      return artists
+    artists = (
+        db.query(models.Artists)
+        .filter(models.Artists.Name.like(f"%{artist_name}%"))
+        .all()
+    )
+    if artists:
+        return artists
     raise HTTPException(status_code=404, detail="Artists not found")
 
+
 @app.get("/albums/{id}")
-async def get_albums_by_artist_id(id: int, db: SessionLocal= Depends(get_db)):
+async def get_albums_by_artist_id(artist_id: int, db: SessionLocal = Depends(get_db)):
     """
-    Récupère la liste d'albums d'un artiste donné.
+    Get a list of albums for a given artist.
     """
-    albums = db.query(models.Album).filter(models.Album.ArtistId == id).all()
-    if(albums):
+    albums = db.query(models.Album).filter(models.Album.ArtistId == artist_id).all()
+    if albums:
         return albums
-    raise  HTTPException(status_code=404, detail="No artist found")
+    raise HTTPException(status_code=404, detail="No artist found")
+
 
 @app.get("/tracks/{id}")
-async def get_track_name_by_album_id(id: int, db: SessionLocal= Depends(get_db)):
+async def get_track_name_by_album_id(album_id: int, db: SessionLocal = Depends(get_db)):
     """
-    Récupère la liste des titres d'un album donné.
+    Get a list of titles for a given album.
     """
-    track = db.query(models.Track).filter(models.Track.AlbumId == id).all()
-    if(track):
+    track = db.query(models.Track).filter(models.Track.AlbumId == album_id).all()
+    if track:
         return track
-    raise  HTTPException(status_code=404, detail="No album found")
+    raise HTTPException(status_code=404, detail="No album found")
+
+
+@app.get("/top_tracks/")
+async def get_top_tracks(db: SessionLocal = Depends(get_db)):
+    """
+    Get the top tracks based on the number of customers who have purchased them.
+    """
+    top_tracks = (
+        db.query(
+            models.Track.Name,
+            func.count(models.Invoices.CustomerId).label("customer_count"),
+        )
+        .join(
+            models.Invoice_items, models.Invoice_items.TrackId == models.Track.TrackId
+        )
+        .join(
+            models.Invoices, models.Invoices.InvoiceId == models.Invoice_items.InvoiceId
+        )
+        .group_by(models.Track.Name)
+        .order_by(func.count(models.Invoices.CustomerId).desc())
+        .limit(3)
+        .all()
+    )
+    top_tracks_data = [
+        {"TrackName": name, "CustomerCount": count} for name, count in top_tracks
+    ]
+    if top_tracks:
+        return top_tracks_data
+    else:
+        raise HTTPException(status_code=404, detail="No top tracks found")
--- server/models.py
+++ server/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import  Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 from .database import Base
 
@@ -9,19 +9,22 @@ class Artists(Base):
     Artistid = Column(Integer, primary_key=True)
     Name = Column(String)
 
+
 class Genres(Base):
     __tablename__ = "genres"
 
     Genreid = Column(Integer, primary_key=True)
     Name = Column(String)
 
+
 class Album(Base):
     __tablename__ = "albums"
 
     AlbumId = Column(Integer, primary_key=True)
     Title = Column(String)
-    ArtistId = Column(Integer, ForeignKey('artists.Artistid')) # L'identifiant de l'artiste associé à l'album.
-    Artist = relationship("Artists")
+    ArtistId = Column(Integer, ForeignKey("artists.Artistid"))
+    Artist = relationship("Artists")  # Identifies the artist associated with the album.
+
 
 class Track(Base):
     __tablename__ = "tracks"
@@ -30,5 +33,37 @@ class Track(Base):
     Name = Column(String)
     UnitPrice = Column(Integer)
     Composer = Column(String)
-    AlbumId = Column(Integer, ForeignKey('albums.AlbumId'))
-    album = relationship("Album", primaryjoin=AlbumId == Album.AlbumId)  # L'identifiant de l'album auquel la piste est associée.
+    AlbumId = Column(Integer, ForeignKey("albums.AlbumId"))
+    album = relationship(
+        "Album", primaryjoin=AlbumId == Album.AlbumId
+    )  # Identifies the album to which the track is associated.
+
+
+class Customers(Base):
+    __tablename__ = "customers"
+
+    CustomerId = Column(Integer, primary_key=True)
+    FirstName = Column(String)
+    LastName = Column(String)
+    Country = Column(String)
+
+
+class Invoices(Base):
+    __tablename__ = "invoices"
+
+    InvoiceId = Column(Integer, primary_key=True)
+    CustomerId = Column(Integer, ForeignKey("customers.CustomerId"))
+    custom = relationship(
+        "Customers", primaryjoin=CustomerId == Customers.CustomerId
+    )  # Relates invoices to customers.
+
+
+class Invoice_items(Base):
+    __tablename__ = "invoice_items"
+
+    InvoiceItemId = Column(Integer, primary_key=True)
+    InvoiceId = Column(Integer, ForeignKey("invoices.InvoiceId"))
+    TrackId = Column(Integer, ForeignKey("tracks.TrackId"))
+    custom = relationship(
+        "Invoices", primaryjoin=InvoiceId == Invoices.InvoiceId
+    )  # Relates invoice items to invoices.
