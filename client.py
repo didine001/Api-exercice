cfb0bbed0bd7833e0006f6bdd7240b9f97142fff
didine712 <aguenassia@gmail.com>
2023-10-27 14:03:45 +0200

    comments added

--- client.py
+++ client.py
@@ -3,40 +3,47 @@ import requests
 FAST_API_URL = "http://127.0.0.1:8000"
 
 
+# Function to get artists by name.
 def get_artists_by_name(artist_name):
     response = requests.get(
         f"{FAST_API_URL}/artists/{artist_name}"
-    )  # we use the endpoint created in api.py and we use the corresponding route to get the data.
+    )  # Send a GET request to the artists endpoint with the provided name
     return (
         response.json()
     )  # We format on json because if we don't it returns an object.
 
 
+# Function to read genres.
 def read_genres():
     response = requests.get(f"{FAST_API_URL}/genres/")
     return response.json()
 
 
+# Function to get album artist id.
 def get_albums_by_artist_id(artist_id):
     response = requests.get(f"{FAST_API_URL}/albums/{artist_id}")
     return response.json()
 
 
+# Function to get track name by album id.
 def get_track_name_by_album_id(album_id):
     response = requests.get(f"{FAST_API_URL}/tracks/{album_id}")
     return response.json()
 
 
+# Function to delete the artist.
 def delete_artist(artist_id):
     response = requests.delete(f"{FAST_API_URL}/artists/{artist_id}")
     return response.json()
 
 
+# Function to update the artist name.
 def update_artist_name(artist_id, name):
     response = requests.put(f"{FAST_API_URL}/artists/{artist_id}?name={name}")
     return response.json()
 
 
+# Function to create an artist.
 def create_artist(name):
     response = requests.post(f"{FAST_API_URL}/artists/?new_name={name}")
     return response.json()
@@ -58,11 +65,11 @@ while True:
     user_choice = input(choix)
 
     if user_choice == "q":
-        break  # Sortir de la boucle puisqu'on veut quitter
+        break  # Escape the loop by pressing q
 
     match user_choice:
         case "1":
-            artist_name = input("Entrez le nom de l'artiste : ")
+            artist_name = input("Enter the name of the artist : ")
             result = get_artists_by_name(artist_name)
             print(result)
 
@@ -71,27 +78,27 @@ while True:
             print(result)
 
         case "3":
-            artist_id = input("Entrez l'ID de l'artiste : ")
+            artist_id = input("Enter the artist id : ")
             result = get_albums_by_artist_id(artist_id)
             print(result)
 
         case "4":
-            album_id = input("Entrez l'ID de l'album : ")
+            album_id = input("Enter the album id : ")
             result = get_track_name_by_album_id(album_id)
             print(result)
 
         case "5":
-            artist_id = input("Entrez l'ID de l'artiste : ")
+            artist_id = input("Enter the artist id : ")
             result = delete_artist(artist_id)
 
         case "6":
-            artist_id = input("Entrez l'ID de l'artiste : ")
-            name = input("Entrez le nouveau nom de l'artiste : ")
+            artist_id = input("Enter the artist id : ")
+            name = input("Enter the new name of the artist : ")
             result = update_artist_name(artist_id, name)
 
         case "7":
-            name = input("Entrez le nom de l'artiste : ")
+            name = input("Enter the name of the artist : ")
             result = create_artist(name)
 
         case _:
-            print("Option invalide. Veuillez choisir une option valide.")
+            print("Invalid option, select a valid one.")
--- test_serveur.py
+++ test_serveur.py
@@ -5,12 +5,14 @@ from server import app
 client = TestClient(app)
 
 
+# Test reading genres.
 def test_read_genres():
     response = client.get("/genres/")
     assert response.status_code == 200
     assert len(response.json()) > 0
 
 
+# Test searching for artists by name.
 def test_search_artists_by_name():
     artist_name = "Accept"
     response = client.get(f"/artists/{artist_name}")
@@ -19,55 +21,64 @@ def test_search_artists_by_name():
     assert expected_data in response.json()
 
 
+# Test searching for artists with an invalid name.
 def test_search_artists_not_valid_name():
     artist_name = "Halliday"
     response = client.get(f"/artists/{artist_name}")
     assert response.status_code == 404
 
 
+# Test getting track names by album ID.
 def test_get_track_name_by_album_id():
     album_id = "5"
     response = client.get(f"/tracks/{album_id}")
     assert response.status_code == 200
 
 
+# Test getting track names with an invalid album ID.
 def test_get_track_name_by_album_id_invalid():
     album_id = "1000"
     response = client.get(f"/tracks/{album_id}")
     assert response.status_code == 404
 
 
+# Test getting albums by artist ID.
 def test_get_album_by_artist_id():
     artist_id = "2"
     response = client.get(f"/tracks/{artist_id}")
     assert response.status_code == 200
 
 
+# Test getting albums with an invalid artist ID.
 def test_get_album_by_artist_id_invalid():
     artist_id = "2000"
     response = client.get(f"/tracks/{artist_id}")
     assert response.status_code == 404
 
 
+# Test deleting an artist by ID.
 def test_delete_artist():
     artist_id = "258"
     response = client.delete(f"/artists/{artist_id}")
     assert response.status_code == 200
 
 
+# Test deleting an artist with an invalid ID.
 def test_delete_artist_invalid():
     artist_id = "5070"
     response = client.delete(f"/artists/{artist_id}")
     assert response.status_code == 404
 
 
+# Test updating an artist's name by ID.
 def test_update_artist_name():
     artist_id = "55"
-    new_name = "The weeknd"
+    new_name = "The Weeknd"
     response = client.put(f"/artists/{artist_id}?name={new_name}")
     assert response.status_code == 200
 
 
+# Test updating an artist's name with an invalid ID.
 def test_update_artist_name_invalid():
     artist_id = "9999"
     new_name = "John"
@@ -75,13 +86,15 @@ def test_update_artist_name_invalid():
     assert response.status_code == 404
 
 
+# Test creating a new artist with a valid name.
 def test_create_artist():
-    new_name = "the jones"
+    new_name = "The Jones"
     response = client.post(f"/artists/?new_name={new_name}")
     assert response.status_code == 200
 
 
+# Test creating a new artist with an invalid name.
 def test_create_artist_invalid():
-    new_name = "weekendes"
+    new_name = "Weekendes"
     response = client.post(f"/artists/?new_name={new_name}")
     assert response.status_code == 404
